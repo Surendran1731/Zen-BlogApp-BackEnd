fb2cd96efb63e7c3da779297cbb068c6592c6ecd
Surendran1731 <143333240+Surendran1731@users.noreply.github.com>
2024-07-18 11:05:47 +0530

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -38,8 +38,11 @@ router.post("/login",async (req,res)=>{
         }
         const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
         const {password,...info}=user._doc
-        res.cookie("token",token).status(200).json(info)
-
+        // res.cookie("token",token,).status(200).json(info)
+        res.cookie("token",token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV,
+        }).status(200).json(info)
     }
     catch(err){
         res.status(500).json(err)
